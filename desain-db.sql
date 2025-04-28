2e7bad3ef0df2dce5195f94fda31bb4a28845ffd
suwantosanjaya <suwantosanjaya@gmail.com>
2025-04-28 14:27:33 +0700

    tambah register dan login

--- desain-db.sql
+++ desain-db.sql
@@ -10,3 +10,12 @@ CREATE TABLE mahasiswa (
 	jk VARCHAR(1),
 	PRIMARY KEY(nim)
 );
+
+CREATE TABLE pengguna(
+	uname VARCHAR(30) NOT NULL,
+	pwd VARCHAR(32) NOT NULL,
+	fname VARCHAR(50),
+	email VARCHAR(100) NOT NULL UNIQUE,
+	akses INTEGER DEFAULT 2,
+	PRIMARY KEY (uname)
+);
